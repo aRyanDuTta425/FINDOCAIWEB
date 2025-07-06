8e3a1738de51d4ac20cc06470430f1427dcc97e5
Aryan Dutta <aryandutta425@gmail.com>
2025-07-06 20:51:42 +0530

    Fix Vercel deployment: simplify next.config, move large files, improve .vercelignore

--- next.config.js
+++ next.config.js
@@ -6,15 +6,7 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['tesseract.js', 'canvas'],
   },
-  // Use webpack configuration to avoid file tracing issues
-  webpack: (config) => {
-    // Disable webpack watch options that can cause issues with file patterns
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/node_modules/**', '**/.git/**', '**/.next/**']
-    }
-    return config
-  },
+  output: 'standalone',
 }
 
 module.exports = nextConfig
